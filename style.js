82023f1cf23862ae5f6183cd9528aea659ae2922
Husam Ahalim <s.ahalim@yahoo.com>
2020-01-29 12:51:13 -0800

    js style sheet and local storage fix

---
+++ style.js
@@ -0,0 +1,286 @@
+var questions = [{
+    question: "1. What is JavaScript?",
+    choices: ["A Way to Order coffee", "StarBucks Secret Menue", "How People Write after to Much Coffee", "A programming Language"],
+    correctAnswer: 3
+}, {
+    question: "2. How to empty an array in JavaScript?",
+    choices: ["arrayList[]", "arrayList(0)", "arrayList.length=0", "arrayList.len(0)"],
+    correctAnswer: 2
+}, {
+    question: "3. JavaScript is the same as Java?",
+    choices: ["True", "False", "Maybe", "No One Knows"],
+    correctAnswer: 1
+}, {
+    question: "4. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
+    choices: ["undefined", "0", "prints nothing", "Syntax error"],
+    correctAnswer: 0
+}, {
+    question: "5. How do you write `Hello World` in an alert box?",
+    choices: ["alert(`Hello World`)", "msgBox(`Hello World`)", "msg(`Hello World`)"],
+    correctAnswer: 0
+},{
+	question: "6. Which software company developed JavaScript?",
+    choices: ["Mozilla", "Netscape", "Sun Microsystems", "Oracle"],
+    correctAnswer: 1
+},{
+	question: "7. What would be the result of 3+2+'7'?",
+    choices: ["327", "12", "14", "57"],
+    correctAnswer: 3
+},{
+	question: "8. In a fight Between Batman and Superman. Who Would Win?",
+    choices: ["We All Lose", "Batman", "Movie Fans Because That Movie Sucked", "Superman"],
+    correctAnswer: 2
+},{
+	question: "9. How can a value be appended to an array?",
+    choices: ["arr(length).value;", "arr[arr.length]=value;", "arr[]=add(value);", "None of these"],
+    correctAnswer: 1
+},{
+	question: "10. What will the code below output to the console? console.log(1 +  +'2' + '2');",
+    choices: ["'32'", "'122'", "'13'", "'14'"],
+    correctAnswer: 0
+}];
+
+
+
+var currentQuestion = 0;
+var viewingAns = 0;
+var correctAnswers = 0;
+var quizOver = false;
+var iSelectedAnswer = [];
+	var c=180;
+	var t;
+$(document).ready(function () 
+{
+    // Display the first question
+    displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
+    $(this).find(".preButton").attr('disabled', 'disabled');
+	
+	timedCount();
+	
+	$(this).find(".preButton").on("click", function () 
+	{		
+		
+        if (!quizOver) 
+		{
+			if(currentQuestion == 0) { return false; }
+	
+			if(currentQuestion == 1) {
+			  $(".preButton").attr('disabled', 'disabled');
+			}
+			
+				currentQuestion--; 
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					
+				} 					
+		} else {
+			if(viewingAns == 3) { return false; }
+			currentQuestion = 0; viewingAns = 3;
+			viewResults();		
+		}
+    });
+
+	
+	// On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function () 
+	{
+        if (!quizOver) 
+		{
+			
+            var val = $("input[type='radio']:checked").val();
+
+            if (val == undefined) 
+			{
+                $(document).find(".quizMessage").text("Try Again");
+                $(document).find(".quizMessage").show();
+            } 
+			else 
+			{
+                
+                $(document).find(".quizMessage").hide();
+				if (val == questions[currentQuestion].correctAnswer) 
+				{
+					correctAnswers++;
+				}
+				iSelectedAnswer[currentQuestion] = val;
+				
+				currentQuestion++; 
+				if(currentQuestion >= 1) {
+					  $('.preButton').prop("disabled", false);
+				}
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					
+				} 
+				else 
+				{
+					// score display and play again btn
+					displayScore();
+					$('#iTimeShow').html('Quiz Completed!');
+					$('#timer').html("You scored: " + correctAnswers + " out of: " + questions.length);
+					c=185;
+					$(document).find(".nextButton").text("Play Again?");
+					quizOver = true;
+					return false;
+					
+				}
+			}
+					
+		}	
+		else 
+		{ 
+			quizOver = false; $('#iTimeShow').html('Time Remaining:'); iSelectedAnswer = [];
+			$(document).find(".nextButton").text("Next Question");
+			$(document).find(".preButton").text("Previous Question");
+			 $(".preButton").attr('disabled', 'disabled');
+			resetQuiz();
+			viewingAns = 1;
+			displayCurrentQuestion();
+			hideScore();
+		}
+    });
+});
+
+
+
+function timedCount()
+	{
+		if(c == 185) 
+		{ 
+			return false; 
+		}
+		
+		var hours = parseInt( c / 3600 ) % 24;
+		var minutes = parseInt( c / 60 ) % 60;
+		var seconds = c % 60;
+		var result = (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);            
+		$('#timer').html(result);
+		
+		if(c == 0 )
+		// final core
+		{
+					displayScore();
+					$('#iTimeShow').html('Quiz Time Completed!');
+					$('#timer').html("You scored: " + correctAnswers + " out of: " + questions.length);
+					c=185;
+					$(document).find(".nextButton").text("Play Again?");
+					quizOver = true;
+					return false;
+					
+		}
+		
+	
+		c = c - 1;
+		t = setTimeout(function()
+		{
+			timedCount()
+		},1000);
+	}
+	
+	
+
+function displayCurrentQuestion() 
+{
+
+	if(c == 185) { c = 180; timedCount(); }
+   
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+
+    $(questionClass).text(question);
+   
+    $(choiceList).find("li").remove();
+    var choice;
+	
+	
+    for (i = 0; i < numChoices; i++) 
+	{
+        choice = questions[currentQuestion].choices[i];
+		
+		if(iSelectedAnswer[currentQuestion] == i) {
+			$('<li><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+		} else {
+			$('<li><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+		}
+    }
+}
+
+function resetQuiz()
+{
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore()
+{
+    $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() 
+{
+    $(document).find(".result").hide();
+}
+
+// displays current question & choices
+function viewResults() 
+{
+
+	if(currentQuestion == 10) { currentQuestion = 0;return false; }
+	if(viewingAns == 1) { return false; }
+
+	hideScore();
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+  tion
+    $(questionClass).text(question);
+  
+    $(choiceList).find("li").remove();
+    var choice;
+	
+	
+	for (i = 0; i < numChoices; i++) 
+	{
+        choice = questions[currentQuestion].choices[i];
+		
+		if(iSelectedAnswer[currentQuestion] == i) {
+			if(questions[currentQuestion].correctAnswer == i) {
+				$('<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			} else {
+				$('<li style="border:2px solid red;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			}
+		} else {
+			if(questions[currentQuestion].correctAnswer == i) {
+				$('<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			} else {
+				$('<li><input type="radio" class="radio-inline" value=' + i + ' name="user" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
+			}
+		}
+    }
+	
+	currentQuestion++;
+	
+	setTimeout(function()
+		{
+			viewResults();
+		},3000);
+}
+
+
+
+
+
+
+var the_results= [];
+the_results = prompt("Enter Initnals? to begin");
+localStorage.setItem("Initals",  JSON.stringify(the_results));
+
+//...
+var storedInitals = JSON.parse(localStorage.getItem(displayScore));
